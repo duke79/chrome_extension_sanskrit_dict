9d0e755b7b5ca91dc5e806ddc06cedecf8ef223d
pulkit-singh <pulkit_singh@3dplmsoftware.com>
2016-07-12 18:45:18 +0530

    get selected text (not working, wrong 'document' object)

--- popup.js
+++ popup.js
@@ -1,19 +1,39 @@
-
+function getSelText()
+{
+	var txt='';
+	if (window.getSelection)
+	{
+//		window.alert('txt='+window.getSelection());
+		return txt+window.getSelection();
+	}
+	else if (document.getSelection)
+	{
+		return txt+document.getSelection();
+	}
+	else if (document.selection)
+	{
+		return document.selection.createRange().text;
+	}
+	else return 'not found!';            
+}
 
 document.addEventListener('DOMContentLoaded', function() {  
-var dictresult = document.getElementById('dict-result');  
-dictresult.textContent='Extension started';
-
-    document.body.onmouseup = function() {mouseUp()};
-    function mouseUp() {
-       var txt = getSelText();       
-        if (/[\u0900-\u097F]/.test(txt)) {            
-          alert(txt);
-          var url = 'https://spokensanskrit.de/index.php?beginning=0+&tinput='+txt+'&trans=Translate';
-            
-          var httpContent = httpGet(url);          
-            alert('after fetch: '+txt);
-          alert(httpContent);
-        }
-    }       
+	var dictresult = document.getElementById('dict-result');  
+	dictresult.textContent='Extension started';	
+    
+	//chrome.tabs.executeScript(null, {code: 'window.alert("doc="'});
+	//window.alert('doc='+document.documentElement.innerHTML);      
+	
+	var txt = getSelText();       	
+	
+	dictresult.textContent='txt='+txt;
+	if (/[\u0900-\u097F]/.test(txt)) {            
+	  alert(txt);
+	  var url = 'https://spokensanskrit.de/index.php?beginning=0+&tinput='+txt+'&trans=Translate';
+		
+	  var httpContent = httpGet(url);          
+		alert('after fetch: '+txt);
+	  alert(httpContent);
+	}    
+	
 }, false);
